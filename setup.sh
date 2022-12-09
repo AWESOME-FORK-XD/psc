27b894eef2e3a8f6a050c73c347f2b0c94c9ea36
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-12-09 21:50:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,64 +1,10 @@
 clear
-function permit_masuk() {
-	dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-	biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-	BURIQ() {
-		curl -sS https://raw.githubusercontent.com/majrot/kuprit/main/ip.txt > /root/tmp
-		data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-		for user in "${data[@]}"; do
-			exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-			d1=(`date -d "$exp" +%s`)
-			d2=(`date -d "$biji" +%s`)
-			exp2=$(( (d1 - d2) / 86400 ))
-			if [[ "$exp2" -le "0" ]]; then
-				echo $user > /etc/.$user.ini
-			else
-				rm -f /etc/.$user.ini > /dev/null 2>&1
-			fi
-		done
-		rm -f /root/tmp
-	}
-	MYIP=$(curl -sS ipv4.icanhazip.com)
-	Name=$(curl -sS https://raw.githubusercontent.com/majrot/kuprit/main/ip.txt | grep $MYIP | awk '{print $2}')
-	echo $Name > /usr/local/etc/.$Name.ini
-	CekOne=$(cat /usr/local/etc/.$Name.ini)
-	Bloman() {
-		if [ -f "/etc/.$Name.ini" ]; then
-			CekTwo=$(cat /etc/.$Name.ini)
-			if [ "$CekOne" = "$CekTwo" ]; then
-				res="Expired"
-			else
-				res="Permission Accepted..."
-			fi
-		fi
-	}
-	PERMISSION() {
-		MYIP=$(curl -sS ipv4.icanhazip.com)
-		IZIN=$(curl -sS https://raw.githubusercontent.com/majrot/kuprit/main/ip.txt | awk '{print $4}' | grep $MYIP)
-		if [ "$MYIP" = "$IZIN" ]; then
-			Bloman
-		else
-			res="Permission Denied!"
-		fi
-	}
-	BURIQ
-	green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-	red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-	#PERMISSION
-	res="Permission Accepted..."
-	if [ -f /home/needupdate ]; then
-		red "Your script need to update first !"
-		exit 0
-	elif [ "$res" = "Permission Accepted..." ]; then
-		echo -ne
-	else
-		red "Permission Denied!"
-		exit 0
+
 	fi
 }
 
 function import_string() {
-	export SCRIPT_URL='https://raw.githubusercontent.com/majrot/kuprit/main/ssc'
+	export SCRIPT_URL='https://raw.githubusercontent.com/sibeesans/psc/main/ssc'
 	export RED="\033[0;31m"
 	export GREEN="\033[0;32m"
 	export YELLOW="\033[0;33m"
