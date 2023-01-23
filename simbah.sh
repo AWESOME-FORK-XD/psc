0a1d1b54c5983a19d7738b6fb46d8fe68ac95e38
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-01-23 10:37:21 +0700

    Update simbah.sh

--- simbah.sh
+++ simbah.sh
@@ -2,4 +2,4 @@
 
 clear
 #sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 &&
-apt update && apt install -y bzip2 gzip coreutils screen curl wget && wget -q https://raw.githubusercontent.com/majrot/kuprit/main/data/setup.sh && chmod +x setup.sh && ./setup.sh
+apt update && apt install -y bzip2 gzip coreutils screen curl wget && wget -q https://raw.githubusercontent.com/sibeesans/psc/main/data/setup.sh && chmod +x setup.sh && ./setup.sh
